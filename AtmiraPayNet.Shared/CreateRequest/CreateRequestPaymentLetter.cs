6af005ecc98013a8bbf987802b5b6a5701b491b8
pablo.alcudia <pablo.alcudia@AM-COR-POR-005>
2024-05-16 10:01:27 +0200

    cambio realizado aspecto visual de la interfaz

--- AtmiraPayNet.Shared/CreateRequest/CreateRequestPaymentLetter.cs
+++ AtmiraPayNet.Shared/CreateRequest/CreateRequestPaymentLetter.cs
@@ -9,31 +9,31 @@ namespace AtmiraPayNet.Shared.CreateRequest
 {
     public class CreateRequestPaymentLetter
     {
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La cuenta origen en formato IBAN es requerida")]
         public string OriginAccountIBAN { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El nombre del banco origen es obligatorio")]
 
         public string OriginBankName { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El país del banco origen es obligatorio")]
         public string OriginCountryBank { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La divisa del país es obligatoria")]
         public string OriginCurrencyBank { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El CP es obligatorio")]
         public string CP { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La calle de la oficina es obligatoria")]
         public string Street { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El numero de calle es obligatorio")]
         public int NumberStreet { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
         public int PayAmount { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La cuenta destino en formato IBAN es requerida")]
         public string? DestinationAccountIBAN { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El nombre del banco destino es obligatorio")]
         public string? DestinationBankName { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "El pais del banco destino es obligatorio")]
         public string? DestinationCountryBank { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Required(ErrorMessage = "La divisa del pais destino es obligatoria")]
         public string? DestinationCurrencyBank { get; set; }
 
         public string? InterBankAccountIBAN { get; set; }
