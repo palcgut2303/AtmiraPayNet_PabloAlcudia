8ddba90ff0fb2029c0ff75991e3dd2594b5cb0a8
palcgut2303 <146340194+palcgut2303@users.noreply.github.com>
2024-05-27 14:11:12 +0200

    realizar poder descargar el fichero donde indique el usuario

---
+++ AtmiraPayNet.Client/wwwroot/js/custom.js
@@ -0,0 +1,18 @@
+﻿window.customFunctions = {
+    selectFolder: function (dotNetHelper) {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.style.display = 'none';
+        input.setAttribute('webkitdirectory', 'true');
+        input.setAttribute('directory', 'true');
+
+        input.addEventListener('change', function (event) {
+            const folderPath = event.target.files[0].path;
+            dotNetHelper.invokeMethodAsync('FolderSelected', folderPath);
+        });
+
+        document.body.appendChild(input);
+        input.click();
+        document.body.removeChild(input);
+    }
+};
